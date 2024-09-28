64f08c34bbdb7eb8e5d1d5819bcbb7e1b87ce847
Adam Zvara <xzvara01@vutbr.cz>
2024-09-28 10:17:56 -0400

    Add readme, refactor run

--- project1/run.sh
+++ project1/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+URL_PROG='get_url.py'
+PARAM_PROG='get_params.py'
+URL_OUT='url_test.txt'
+
 if [ ! $(which python3) ]; then
     echo "Python3 is not installed. Please install it."
     exit 1
 fi
 
-# Get URLs
-python3 get_url.py > url_test.txt
+# Get URLs and store them in a file
+python3 $URL_PROG > $URL_OUT
 
-# Get parameters of 10 items
-head -n 10 url_test.txt | python3 get_params.py
+# Get parameters of 10 items from retrieved URLs
+head -n 10 $URL_OUT | python3 $PARAM_PROG
